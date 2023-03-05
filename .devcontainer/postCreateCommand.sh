385155f1c6ae103d96e20a603ee98a70e6c5a297
Kentaro Ishikawa <sanbasan.dev@gmail.com>
2023-03-05 12:04:15 +0000

    [WIP] initializing supabase project

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Select a package that is suitable for your environment from <https://github.com/supabase/cli/releases>
+SUPABASECLI_VERSION="1.41.6"
+SUPABASECLI_ARCHITECTURE="amd64" # amd64 or arm64
+SUPABASECLI_FILENAME="supabase_${SUPABASECLI_VERSION}_linux_${SUPABASECLI_ARCHITECTURE}.deb"
+
+set -ex
+
+# Supabase CLI
+curl -LO https://github.com/supabase/cli/releases/download/v$SUPABASECLI_VERSION/$SUPABASECLI_FILENAME
+sudo dpkg -i ./$SUPABASECLI_FILENAME
+rm -rf $SUPABASECLI_FILENAME
